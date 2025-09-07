13e6f30cfb24c8ed4fdf0dc59117b7f145891bcb
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2025-09-07 12:26:26 +0300

    Create TheLoginForm and LoginView

---
+++ src/components/TheLoginForm.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <form class="border rounded-md">
+    <h3 class="bg-sky-700 text-white text-center px-3 py-2">Login</h3>
+    <div class="flex flex-col px-3 py-2">
+      <input
+        type="text"
+        placeholder="Login"
+        class="border border-sky-700 hover:ring ring-sky-700 rounded-sm p-1 mb-2"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        class="border border-sky-700 hover:ring ring-sky-700 rounded-sm p-1 mb-2"
+      />
+      <button
+        class="border rounded-full bg-emerald-400 hover:bg-emerald-500 border-emerald-400 text-white w-fit px-4 py-1 self-center"
+        type="submit"
+      >
+        Send
+      </button>
+    </div>
+  </form>
+</template>
+
+<style>
+@import 'tailwindcss';
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TheLoginForm from '@/components/TheLoginForm.vue'
+</script>
+
+<template>
+  <main class="w-sm mx-auto h-full grid grid-rows-[auto_1fr]">
+    <h1 class="text-4xl text-center mb-4">Vue3 demo project</h1>
+    <div class="self-center">
+      <TheLoginForm />
+    </div>
+  </main>
+</template>
+
+<style>
+@import 'tailwindcss';
+</style>
