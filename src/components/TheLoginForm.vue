d00f85d2563c2375aba3a089a385eb680628ef82
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2025-09-17 14:32:16 +0300

    Add validation and use BaseInput on the Login form

--- src/components/TheLoginForm.vue
+++ src/components/TheLoginForm.vue
@@ -1,18 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRegle } from '@regle/core'
+import { required } from '@regle/rules'
+import { ref } from 'vue'
+import BaseInput from './controls/BaseInput.vue'
+
+const formData = ref({
+  login: '',
+  password: '',
+})
+
+const validationRules = {
+  login: { required },
+  password: { required },
+}
+
+const { r$ } = useRegle(formData, validationRules)
+
+const onSubmit = async () => {
+  console.log('onSubmit formData:', formData.value)
+  const { valid } = await r$.$validate()
+  if (!valid) {
+    console.error('onSubmit form has errors:', r$, r$.$errors)
+    return
+  }
+}
+</script>
 
 <template>
-  <form class="border rounded-md">
+  <form class="border rounded-md" @submit.prevent="onSubmit">
     <h3 class="bg-sky-700 text-white text-center px-3 py-2">Login</h3>
     <div class="flex flex-col px-3 py-2">
-      <input
-        type="text"
-        placeholder="Login"
-        class="border border-sky-700 hover:ring ring-sky-700 rounded-sm p-1 mb-2"
-      />
-      <input
-        type="password"
+      <BaseInput v-model="formData.login" placeholder="Login" :errors="r$.login.$errors" />
+      <BaseInput
+        v-model="formData.password"
         placeholder="Password"
-        class="border border-sky-700 hover:ring ring-sky-700 rounded-sm p-1 mb-2"
+        :errors="r$.password.$errors"
+        type="password"
       />
       <button
         class="border rounded-full bg-emerald-400 hover:bg-emerald-500 border-emerald-400 text-white w-fit px-4 py-1 self-center"
