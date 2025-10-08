0b8cb21af05771022fb653650db889c86eb3be64
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2025-10-09 01:14:29 +0300

    Add the `404` route

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="w-full h-full flex items-center">
+    <div class="w-full text-center text-orange-500">
+      Ooops... Something went wrong.
+    </div>
+  </main>
+</template>
