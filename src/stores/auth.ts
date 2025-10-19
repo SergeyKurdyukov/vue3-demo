5069a42e019ec3c4949253abb2798e06a1757950
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2025-10-19 21:16:18 +0300

    Save the session into sessionStorage

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -2,18 +2,25 @@ import { defineStore } from 'pinia'
 import authApi from '@/api/auth'
 import { computed, ref } from 'vue'
 
+const SESSION_KEY = 'sessionKey'
+
 export const useAuthStore = defineStore('auth', () => {
   const error = ref()
   const session = ref()
   const pendings = ref({
     login: false,
   })
-  const isUserLoggedIn = computed(() => !!session.value)
+
+  const isUserLoggedIn = computed(() => {
+    const savedSession = sessionStorage.getItem(SESSION_KEY)
+    return savedSession ? JSON.parse(savedSession) : null
+  })
 
   const login = async (params: { email: string; password: string }) => {
     try {
       pendings.value.login = true
       session.value = await authApi.login(params)
+      sessionStorage.setItem(SESSION_KEY, JSON.stringify(session.value))
     } catch (e: any) {
       error.value = e.body
     } finally {
