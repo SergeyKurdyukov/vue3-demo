f20ca6a18e41e4b0e38837882557e4b8a1107e0b
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2025-10-19 14:02:00 +0300

    Add new views

---
+++ src/views/CartView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="cart">
+    <h1>CartView</h1>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/MyProfileView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="my-profile">
+    <h1>MyProfileView</h1>
+  </div>
+</template>
+
+<style></style>
