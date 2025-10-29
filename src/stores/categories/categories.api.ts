4eb2560381dd726de08fda905cebe052499c454d
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2025-10-29 19:58:32 +0300

    Add the default export

--- src/stores/categories/categories.api.ts
+++ src/stores/categories/categories.api.ts
@@ -1,6 +1,11 @@
 import api from '../../api'
-import type { ICategory } from './categories.types'
+import type { ICategory, IProduct } from './categories.types'
 
 const categories = api('/categories')
+const products = api('/products')
 
 export const getCategories = () => categories.get<ICategory[]>()
+
+export default {
+  getCategories,
+}
