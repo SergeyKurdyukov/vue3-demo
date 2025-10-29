3cfbe929b39f115cd5c3a51a1562d41c72d3eed8
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2025-10-29 20:00:05 +0300

    Add filtered categories, group categories data

--- src/stores/categories/categories.ts
+++ src/stores/categories/categories.ts
@@ -1,26 +1,46 @@
-import { getCategories } from '@/stores/categories/categories.api'
+import api from '@/stores/categories/categories.api'
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import type { ICategory } from './categories.types'
 
 export const useCategoriesStore = defineStore('categories', () => {
   const pendings = ref({
     getCategories: false,
   })
 
+  const categories = ref<ICategory[]>([])
+
+  const filteredCategories = computed(() => {
+    return categories.value.filter((category) => {
+      return !(
+        category.image.includes('abstract') ||
+        category.image.includes('placehold') ||
+        category.name.includes('test') ||
+        category.name.includes('undefined') ||
+        category.name.includes('string')
+      )
+    })
+  })
+
+  const getCategories = async () => {
+    try {
+      pendings.value.getCategories = true
+      const result = await api.getCategories()
+      categories.value = result
+      return result
+    } catch (e: any) {
+      console.error('Categories store.getCategories', e)
+      return []
+    } finally {
+      pendings.value.getCategories = false
+    }
+  }
+
   return {
     pendings,
 
-    getCategories: async () => {
-      try {
-        pendings.value.getCategories = true
-        const result = await getCategories()
-        return result
-      } catch (e: any) {
-        console.error('Categories store.getCategories', e)
-        return []
-      } finally {
-        pendings.value.getCategories = false
-      }
-    },
+    categories,
+    filteredCategories,
+    getCategories,
   }
 })
