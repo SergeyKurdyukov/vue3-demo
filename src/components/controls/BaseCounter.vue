f5ce8e4cd3d084a64e2397f15667cd394a9875d8
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2025-11-14 21:11:35 +0300

    Disable if no product selected

--- src/components/controls/BaseCounter.vue
+++ src/components/controls/BaseCounter.vue
@@ -4,11 +4,18 @@ const count = defineModel<number>('count', { type: Number, default: 0 })
 
 <template>
   <div class="w-max border border-stone-400 rounded-sm">
-    <button @click="count--" class="w-5 bg-stone-400 hover:bg-stone-500">
+    <button
+      @click="count--"
+      :disabled="!count"
+      class="w-5 cursor-pointer bg-stone-400 hover:bg-stone-500 disabled:bg-stone-300 disabled:cursor-default"
+    >
       -
     </button>
     <input disabled v-model="count" class="w-10 text-center" />
-    <button @click="count++" class="w-5 bg-stone-400 hover:bg-stone-500">
+    <button
+      @click="count++"
+      class="w-5 cursor-pointer bg-stone-400 hover:bg-stone-500"
+    >
       +
     </button>
   </div>
