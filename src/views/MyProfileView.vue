7c25ccfad28cf039b7486407d794e72090774ad2
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2025-11-17 00:57:09 +0300

    Show user details

--- src/views/MyProfileView.vue
+++ src/views/MyProfileView.vue
@@ -1,6 +1,23 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth/auth'
+
+const authStore = useAuthStore()
+authStore.getUser()
+</script>
+
 <template>
   <div class="my-profile">
-    <h1>MyProfileView</h1>
+    <h1 class="text-xl font-bold my-4">MyProfileView</h1>
+
+    <p v-if="!authStore.currentUser">No user data</p>
+    <div v-else class="flex">
+      <img :src="authStore.currentUser.avatar" alt="User photo" class="w-40" />
+      <div class="px-4">
+        <p>Name: {{ authStore.currentUser.name }}</p>
+        <p>Email: {{ authStore.currentUser.email }}</p>
+        <p>Registration date: {{ authStore.currentUser.creationAt }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
