9713d7ccf4da5be096e78b7d89328b8484d98d95
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2025-11-16 19:49:09 +0300

    Add `Transition` for the Product list

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -18,41 +18,50 @@ const onCountUpdate = (value: number, product: IProductCart) => {
   <div>
     <h2 class="text-lg font-bold m-4">Products</h2>
 
-    <img
-      v-if="categoriesStore.pendings.getProducts"
-      alt="Progress wheel"
-      class="w-12"
-      src="@/assets/spinner.svg"
-    />
-    <ul
-      v-else-if="categoriesStore.preparedProducts.length"
-      class="flex flex-wrap"
+    <Transition
+      name="slide-right"
+      mode="out-in"
+      enter-from-class="opacity-0 translate-x-10"
+      enter-active-class="transition-all duration-300 ease-out"
+      leave-active-class="transition-all duration-300 ease-out"
+      leave-to-class="opacity-0 -translate-x-10"
     >
-      <li
-        v-for="product in categoriesStore.preparedProducts"
-        :key="product.id"
-        class="flex flex-col justify-between gap-2 p-2 m-4 w-45 rounded-md border-1 hover:border-blue-400 hover:bg-blue-200 hover:text-yellow-700 transition-all delay-150 duration-300 ease-in-out relative"
+      <img
+        v-if="categoriesStore.pendings.getProducts"
+        alt="Progress wheel"
+        class="w-12 mt-7"
+        src="@/assets/spinner.svg"
+      />
+      <ul
+        v-else-if="categoriesStore.preparedProducts.length"
+        class="flex flex-wrap"
       >
-        {{ product.title }}
-        <button
-          v-if="!product.count"
-          class="bg-orange-500/50 flex items-baseline justify-center hover:bg-orange-500 text-white text-lg rounded-full w-8 h-8 absolute right-5 transition-colors delay-150 duration-300 ease-in-out"
-          @click="cartStore.onAddToCart(product)"
+        <li
+          v-for="product in categoriesStore.preparedProducts"
+          :key="product.id"
+          class="flex flex-col justify-between gap-2 p-2 m-4 w-45 rounded-md border-1 hover:border-blue-400 hover:bg-blue-200 hover:text-yellow-700 transition-all delay-150 duration-300 ease-in-out relative"
         >
-          +
-        </button>
-        <BaseCounter
-          v-else
-          v-model:count="product.count"
-          @update:count="onCountUpdate($event, product)"
-          class="absolute top-2 right-2"
-        />
+          {{ product.title }}
+          <button
+            v-if="!product.count"
+            class="bg-orange-500/50 flex items-baseline justify-center hover:bg-orange-500 text-white text-lg rounded-full w-8 h-8 absolute right-5 transition-colors delay-150 duration-300 ease-in-out"
+            @click="cartStore.onAddToCart(product)"
+          >
+            +
+          </button>
+          <BaseCounter
+            v-else
+            v-model:count="product.count"
+            @update:count="onCountUpdate($event, product)"
+            class="absolute top-2 right-2"
+          />
 
-        <img :src="product.images[0]" alt="Product image" />
-      </li>
-    </ul>
-    <p v-else class="text-center">
-      Oops... Could you please select any other category?
-    </p>
+          <img :src="product.images[0]" alt="Product image" />
+        </li>
+      </ul>
+      <p v-else class="text-center">
+        Oops... Could you please select any other category?
+      </p>
+    </Transition>
   </div>
 </template>
