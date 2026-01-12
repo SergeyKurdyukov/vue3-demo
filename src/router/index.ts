d471f96f585b1d200b3124acdcaa6d2a9b7961c6
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2026-01-13 01:27:29 +0300

    Remove `AboutView`

--- src/router/index.ts
+++ src/router/index.ts
@@ -36,15 +36,6 @@ const router = createRouter({
       ],
     },
     {
-      path: '/about',
-      name: 'about',
-      meta: { requiresAuth: true },
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/:pathMatch(.*)*',
       name: 'NotFound',
       component: () => import('../views/NotFoundView.vue'),
