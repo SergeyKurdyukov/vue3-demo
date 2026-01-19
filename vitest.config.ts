355da895188e1267b93493ef63134518d9951cd5
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2026-01-20 01:22:34 +0300

    Configure `vitest` browser mode

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
+import { playwright } from '@vitest/browser-playwright'
 
 export default mergeConfig(
   viteConfig,
@@ -9,6 +10,12 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      browser: {
+        provider: playwright(),
+        enabled: true,
+        // at least one instance is required
+        instances: [{ browser: 'chromium' }],
+      },
     },
   }),
 )
