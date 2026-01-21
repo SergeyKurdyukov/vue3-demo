ac90f1b3d7ddb9260bac14da9b0e0b96991f4d39
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2026-01-21 18:42:56 +0300

    Minor change for the case with several elements

--- src/components/__tests__/TheLoginForm.test.ts
+++ src/components/__tests__/TheLoginForm.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'vitest'
 import { render } from 'vitest-browser-vue'
 import TheLoginForm from '@/components/TheLoginForm.vue'
 
-describe('Login tests', () => {
+describe('Login tests', async () => {
   test('Check login form elements', async () => {
     const screen = render(TheLoginForm)
     await expect.element(screen.getByText('Login form')).toBeInTheDocument()
@@ -14,8 +14,10 @@ describe('Login tests', () => {
   test('Check empty fields processing', async () => {
     const screen = render(TheLoginForm)
     await screen.getByText('Send').click()
-    await expect(screen.getByText('This field is required')).toHaveLength(2)
-
+    const errorInputs = screen.container.querySelectorAll(
+      'input.border-red-500',
+    )
+    expect(errorInputs.length).toBe(2)
     await expect
       .element(screen.getByLabelText('Login'))
       .toHaveClass('border-red-500')
