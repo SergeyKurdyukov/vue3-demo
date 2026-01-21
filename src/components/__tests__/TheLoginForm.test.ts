233d05879f7e36372343e57327fd89e6df9f47ca
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2026-01-21 18:21:20 +0300

    Fix 'Check empty fields processing' text. Add `describe`.

--- src/components/__tests__/TheLoginForm.test.ts
+++ src/components/__tests__/TheLoginForm.test.ts
@@ -1,26 +1,26 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { render } from 'vitest-browser-vue'
 import TheLoginForm from '@/components/TheLoginForm.vue'
 
-test('Check login form elements', async () => {
-  const screen = render(TheLoginForm)
-  await expect.element(screen.getByText('Login form')).toBeInTheDocument()
-  await expect.element(screen.getByLabelText('Login')).toBeInTheDocument()
-  await expect.element(screen.getByLabelText('Password')).toBeInTheDocument()
-  await expect.element(screen.getByText('Send')).toBeInTheDocument()
-})
+describe('Login tests', () => {
+  test('Check login form elements', async () => {
+    const screen = render(TheLoginForm)
+    await expect.element(screen.getByText('Login form')).toBeInTheDocument()
+    await expect.element(screen.getByLabelText('Login')).toBeInTheDocument()
+    await expect.element(screen.getByLabelText('Password')).toBeInTheDocument()
+    await expect.element(screen.getByText('Send')).toBeInTheDocument()
+  })
 
-test('Check empty fields processing', async () => {
-  const screen = render(TheLoginForm)
-  await screen.getByText('Send').click()
-  await expect
-    .element(screen.getByText('This field is required'))
-    .toHaveLength(2)
+  test('Check empty fields processing', async () => {
+    const screen = render(TheLoginForm)
+    await screen.getByText('Send').click()
+    await expect(screen.getByText('This field is required')).toHaveLength(2)
 
-  await expect
-    .element(screen.getByLabelText('Login'))
-    .toHaveClass('border-red-500')
-  await expect
-    .element(screen.getByLabelText('Password'))
-    .toHaveClass('border-red-500')
+    await expect
+      .element(screen.getByLabelText('Login'))
+      .toHaveClass('border-red-500')
+    await expect
+      .element(screen.getByLabelText('Password'))
+      .toHaveClass('border-red-500')
+  })
 })
