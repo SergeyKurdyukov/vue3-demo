26820d5a49d3c9d26199a0d1e86830eb6b962ae9
SergeyKurdyukov <sergey.kurdyukov.v@gmail.com>
2026-01-21 18:09:57 +0300

    Add `Check empty fields processing` test

--- src/components/__tests__/TheLoginForm.test.ts
+++ src/components/__tests__/TheLoginForm.test.ts
@@ -2,10 +2,25 @@ import { expect, test } from 'vitest'
 import { render } from 'vitest-browser-vue'
 import TheLoginForm from '@/components/TheLoginForm.vue'
 
-test('Login form elements', async () => {
+test('Check login form elements', async () => {
   const screen = render(TheLoginForm)
   await expect.element(screen.getByText('Login form')).toBeInTheDocument()
   await expect.element(screen.getByLabelText('Login')).toBeInTheDocument()
   await expect.element(screen.getByLabelText('Password')).toBeInTheDocument()
   await expect.element(screen.getByText('Send')).toBeInTheDocument()
+})
+
+test('Check empty fields processing', async () => {
+  const screen = render(TheLoginForm)
+  await screen.getByText('Send').click()
+  await expect
+    .element(screen.getByText('This field is required'))
+    .toHaveLength(2)
+
+  await expect
+    .element(screen.getByLabelText('Login'))
+    .toHaveClass('border-red-500')
+  await expect
+    .element(screen.getByLabelText('Password'))
+    .toHaveClass('border-red-500')
 })
